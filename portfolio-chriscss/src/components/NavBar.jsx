b0e24192f17e069b4a6f1de3fd9f5e68570dca20
Chris-css <christelle.savoca@gmail.com>
2020-07-23 14:51:24 +0200

    installation NavBar

---
+++ portfolio-chriscss/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logoSvg from "../../src/logo.svg";
+import "../style/NavBar.css";
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <img
+                    src={logoSvg}
+                    className="navbar-logo"
+                    alt="logo Chris-css"
+                />
+            </Link>
+            <div className="navbar-menu">
+            <Link to="/">available for hire</Link>
+            <Link to="/">
+                <div className="menu-icon-box">
+                    <i className="fas fa-bars"></i>
+                </div>
+            </Link>
+            </div>  
+        </nav>
+    );
+}
+
+export default NavBar;
